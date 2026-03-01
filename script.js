d0043aab9b8843a33d06b8dfeaec726ce79f3184
Sushil Chauhan <35929917+SushilChauhan28@users.noreply.github.com>
2026-03-01 12:26:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.insertAdjacentHTML("afterbegin",
+        "<div style='text-align:center;padding:5px;border-bottom:1px solid black;'>Website built with Accessibility in mind</div>"
+    );
+});
