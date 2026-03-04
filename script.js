205dfcfa13327ae9bd00693db46d85a84fcd96a9
Sushil Chauhan <35929917+SushilChauhan28@users.noreply.github.com>
2026-03-04 19:47:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.insertAdjacentHTML("afterbegin",
-        "<div style='text-align:center;padding:5px;border-bottom:1px solid black;'>Website built with Accessibility in mind</div>"
-    );
-});
